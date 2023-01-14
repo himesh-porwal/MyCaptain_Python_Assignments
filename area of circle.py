ce5f7a7bd9d2db6ac45cf480cc7224cf0b4216b8
himesh-porwal <122104948+himesh-porwal@users.noreply.github.com>
2023-01-14 19:17:49 +0530

    Add files via upload

---
+++ area of circle.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Himesh Porwal
+My Captain Assignment
+Area of circle
+"""
+
+r= float (input ("Radius of the circle is: "))
+area = 3.14*(r**2)
+
+print ("The area of the circle with radius",r,"is :",area)
