a00670d1e924e52781e407afe22263d77a928e33
Himesh Porwal <122104948+himesh-porwal@users.noreply.github.com>
2023-01-16 22:03:20 +0530

    Add files via upload

---
+++ file extension.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Himesh Porwal
+My Captain Assignment
+Extension of file
+"""
+j= input ("Enter file name with extension: ")
+print ("File name with extension: "+j)
+i= j.split(".")
+print ("File name: "+i[0])
+print ("Extension: "+i[1])
